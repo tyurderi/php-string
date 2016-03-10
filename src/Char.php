3a04b7740dfe501bb5d51bf17d398647121bcd95
Tommy <meier@rhinos-media.de>
2016-03-10 13:51:57 +0100

    more

--- src/Char.php
+++ src/Char.php
@@ -5,6 +5,51 @@ namespace WCKZ\StringUtil;
 class Char
 {
 
-    
+    protected $value    = '';
+
+    protected $position = 0;
+
+    protected $parent   = null;
+
+    protected $prev     = null;
+
+    protected $next     = null;
+
+    public function __construct($value, $position = 0, MutateableString $parent = null)
+    {
+        $this->value    = $value;
+        $this->position = $position;
+        $this->parent   = $parent;
+    }
+
+    public function __toString()
+    {
+        return $this->value;
+    }
+
+    public function prev($prev = null)
+    {
+        if($prev)
+        {
+            $this->prev = $prev;
+        }
+
+        return $this->prev;
+    }
+
+    public function next($next = null)
+    {
+        if($next)
+        {
+            $this->next = $next;
+        }
+
+        return $this->next;
+    }
+
+    public function set($value)
+    {
+        $this->value = $value;
+    }
 
 }
--- src/MutateableString.php
+++ src/MutateableString.php
@@ -2,12 +2,16 @@
 
 namespace WCKZ\StringUtil;
 
-class MutateableString
+include 'Char.php';
+
+class MutateableString implements \ArrayAccess
 {
 
     protected $value  = '';
 
     protected $length = 0;
+
+    protected $chars  = array();
 
     public function __construct($value = null)
     {
@@ -47,14 +51,54 @@ class MutateableString
         return substr($this->value, $start, $length);
     }
 
+    public function update()
+    {
+        if(empty($this->chars))
+        {
+            for($i = 0; $i < $this->length(); $i++)
+            {
+                $char = new Char($this->value[$i], $i, $this);
+                $char->prev($i == 0 ? : $this->chars[$i - 1]);
+
+                if($char->prev())
+                {
+                    $char->next($char);
+                }
+
+                $this->chars[] = $char;
+            }
+        }
+    }
+
+    public function offsetExists($offset)
+    {
+        return $this->offsetGet($offset) !== null;
+    }
+
+    public function offsetGet($offset)
+    {
+        return $this->chars[$offset];
+    }
+
+    public function offsetSet($offset, $value)
+    {
+        if($this->offsetExists($offset))
+        {
+            $this->offsetGet($offset)->set($value);
+        }
+    }
+
+    public function offsetUnset($offset)
+    {
+        //unset($this->chars[$offset]);
+    }
+
     protected function calculateLength()
     {
         $this->length = strlen($this);
+        $this->update();
     }
 
 }
 
 $pattern = new MutateableString('abc+');
-$pattern->add('!');
-
-var_dump($pattern->cut(1));
